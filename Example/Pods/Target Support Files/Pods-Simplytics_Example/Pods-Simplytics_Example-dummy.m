053fc01211f187331dc64229589900a4786e9285
Quinton Wall <qwall@salesforce.com>
2017-12-14 11:10:16 -0800

    removed Pods dir from repo

--- Example/Pods/Target Support Files/Pods-Simplytics_Example/Pods-Simplytics_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Simplytics_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Simplytics_Example
-@end
--- Example/Pods/Target Support Files/Simplytics/Simplytics-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Simplytics : NSObject
-@end
-@implementation PodsDummy_Simplytics
-@end
